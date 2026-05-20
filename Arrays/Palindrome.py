b4dc7116e1a159490dac5ab70b60b759b507715e
bluemau-dev <bluemau-dev@gmail.com>
2026-05-20 03:13:37 +0000

    Another method of completing the problem.

--- Arrays/Palindrome.py
+++ Arrays/Palindrome.py
@@ -1,6 +1,7 @@
 string = "taco cat"
 
-def palindrome(x: type(String)):
+# First Method
+def palindrome_one(x: type(str)):
     # Remove Whitespaces from String
     x = x.replace(" ", "")
 
@@ -17,4 +18,20 @@ def palindrome(x: type(String)):
     else:
         print("Not a Palindrome.") 
 
-palindrome(string)
+def palindrome_two(x: type(str)):
+    # Remove Whitespaces From String
+    x = x.replace(" ", "")
+
+    # Iterate Characters in String
+    w = []
+    for character in x:
+        w.append(character)
+
+    # Palindrome Condition
+    if w == w[::-1]:
+        print("Palindrome")
+    else:
+        print("Not a Palindrome")
+
+palindrome_one(string)
+palindrome_two(string)
